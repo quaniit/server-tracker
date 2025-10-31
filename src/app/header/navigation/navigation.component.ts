7642560bd18681244f305d15bd139f8ca711d842
Jen Funck <Jennifer.Funck3581@gmail.com>
2025-10-31 03:19:56 -0400

    feat(button): Create shared button

--- src/app/header/navigation/navigation.component.ts
+++ src/app/header/navigation/navigation.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "../../shared/button/button.component";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
